a97612b83cca04dee04edda7c843fbc5485daad9
Duncan Malashock <duncanmalashock@gmail.com>
2022-02-15 23:51:46 -0500

    Generate voicing with simple voicing plan

--- src/Step.elm
+++ src/Step.elm
@@ -18,6 +18,9 @@ module Step exposing
 -}
 
 import Id
+import Music.Internal.Placement
+import Music.Internal.Voicing
+import Music.Internal.VoicingPlan
 import Music.Pitch
 import Music.PitchClass
 import Music.Scale
@@ -94,4 +97,39 @@ setPitches newPitches (Step details) =
 
 generateVoicing : Step -> Step
 generateVoicing (Step details) =
-    Step { details | pitches = [] }
+    let
+        voicingPlan =
+            Music.Internal.VoicingPlan.init
+                { scaleType = details.scaleType
+                , selections =
+                    [ Music.Internal.VoicingPlan.select
+                        { options = [ 1 ]
+                        , canBeDoubled = False
+                        , placement = Music.Internal.Placement.placeAnywhere
+                        }
+                    , Music.Internal.VoicingPlan.select
+                        { options = [ 7 ]
+                        , canBeDoubled = False
+                        , placement = Music.Internal.Placement.placeBelow
+                        }
+                    , Music.Internal.VoicingPlan.select
+                        { options = [ 5 ]
+                        , canBeDoubled = False
+                        , placement = Music.Internal.Placement.placeBelow
+                        }
+                    , Music.Internal.VoicingPlan.select
+                        { options = [ 3 ]
+                        , canBeDoubled = False
+                        , placement = Music.Internal.Placement.placeBelow
+                        }
+                    ]
+                }
+    in
+    Step
+        { details
+            | pitches =
+                Music.Internal.VoicingPlan.toVoicings details.scaleRoot voicingPlan
+                    |> List.head
+                    |> Maybe.map Music.Internal.Voicing.toPitches
+                    |> Maybe.withDefault []
+        }
