91c89c416079b455266eeb9f004744a9567cc4cc
Duncan Malashock <duncanmalashock@gmail.com>
2022-03-13 00:43:41 -0500

    Add Music type

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,10 @@ import Html exposing (Html)
 import Html.Attributes as Attr
 import Html.Events
 import Json.Encode
+import Music
+import Music.Key
+import Music.Meter
+import Music.Tempo
 import Piano
 import PianoRoll
 import Ports
@@ -32,6 +36,7 @@ type alias Model =
     , song : Song.Song
     , fileName : String
     , showSaveModal : Bool
+    , music : Music.Music
     }
 
 
@@ -48,6 +53,12 @@ initialModel =
     , song = Song.new
     , fileName = ""
     , showSaveModal = False
+    , music =
+        Music.new
+            { key = Music.Key.c
+            , meter = Music.Meter.fourFour
+            , tempo = Music.Tempo.allegro
+            }
     }
 
 
