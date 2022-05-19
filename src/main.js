d06f2b52f5564517917862b8fbafb1a935f3dfec
username <duncanmalashock@gmail.com>
2022-05-19 19:27:50 -0400

    Remove unnecessary oncontextmenu attribute from body

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import { Elm } from './elm/Main.elm'
 import Ports from './js/ports.js'
 
-document.addEventListener('contextmenu', event => event.preventDefault())
-
+// Initialize the Elm app
 const app = Elm.Main.init()
+
+// Initialize ports
 Ports.init(app)
 
+// Turn off contextual menus on right-click, so that right-clicks can be used
+// for removing notes
+document.addEventListener('contextmenu', event => event.preventDefault())
+
+// scroll C4 into the center of the view
 window.requestAnimationFrame(() => {
-  // scroll C4 into the center of the view
   document.querySelectorAll(".piano__key")[71].scrollIntoView({ block: "center"})
 })
